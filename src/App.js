e3b530221a51ea0b1723998fea09dd03e4af8a02
patrickmwangi017 <mwangipatrick017@gmail.com>
2024-10-24 16:06:59 +0300

    add second section pending css

--- src/App.js
+++ src/App.js
@@ -13,13 +13,41 @@ function App() {
 
       {/* Main content */}
       <main>
-      <div className="row section-1">
-          
+        <div className="row section-1">
+          <div className='col-md-12'>
+            <h1 style={{ color: "#fff", textAlign: 'center', width: '100%', marginTop: '10%', fontWeight: '700', fontSize: '55px' }}>Are You a Supplier?</h1>
+            <h4 style={{ color: "#fff", textAlign: 'center', width: '100%' }}>Explore Matching Opportunities.</h4>
           </div>
+        </div>
         <Container className="my-5">
-       
-          <h1>Welcome to My Website</h1>
-          <p>This is the home page content.</p>
+
+          <div className="row">
+          <h2 className='pb-2' style={{ color: "#000", fontWeight: '700', fontSize: '55px' }}>Ready to dive into <a href='#'> HABOT? </a></h2>
+            <div className='col-md-6'>
+              <p className='pb-2' style={{ color: "#000", textAlign: 'left', width: '100%' }}>Signing up with HABOT opens the door to a world of new opportunities and potential for business growth. Gain access to a vibrant community of like-minded individuals, unlock valuable resources, and take the first step towards realizing your entrepreneurial dreams.</p>
+              <button className='section-2-btn'> Sign up Today ! </button>
+            </div>
+            <div className='col-md-6'>
+              <div className="row mb-2">
+                <div className='col-md-12 d-flex align-items-center justify-content-between'>
+                  <button className='section-2-btn-2'> Sign up Today ! </button>
+                  <button className='section-2-btn-2'> Sign up Today ! </button>
+                </div>
+              </div>
+              <div className="row mb-2">
+                <div className='col-md-12 d-flex align-items-center justify-content-between'>
+                  <button className='section-2-btn-2'> Sign up Today ! </button>
+                  <button className='section-2-btn-2'> Sign up Today ! </button>
+                </div>
+              </div>
+              <div className="row">
+                <div className='col-md-12 d-flex align-items-center justify-content-between'>
+                  <button className='section-2-btn-2'> Sign up Today ! </button>
+                  <button className='section-2-btn-2'> Sign up Today ! </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </Container>
       </main>
 
